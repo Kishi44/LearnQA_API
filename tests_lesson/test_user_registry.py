ad6682fbe83279e467546fd23cb8f056916f2c63
Kishi44 <kishi321@gmail.com>
2024-06-02 14:46:18 +0300

    new test user edit

--- tests_lesson/test_user_registry.py
+++ tests_lesson/test_user_registry.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import requests
 from lib.assertions import Assertions
 from lib.base_case import BaseCase
@@ -7,21 +5,8 @@ from lib.base_case import BaseCase
 
 class TestUserRegister(BaseCase):
 
-    def setup_class(self):
-        base_part = 'learnqa'
-        domain = 'example.com'
-        random_part = datetime.now().strftime('%m%d%Y%H%M%S')
-        self.email = f"{base_part}{random_part}@{domain}"
-
     def test_create_user_successfully(self):
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': self.email
-        }
-
+        data = self.prepeare_registration_data()
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
         Assertions.assert_code_status(response, 200)
@@ -29,13 +14,7 @@ class TestUserRegister(BaseCase):
 
     def test_create_user_with_existing_email(self):
         email = 'vinkotov@example.com'
-        data = {
-            'password': '123',
-            'username': 'learnqa',
-            'firstName': 'learnqa',
-            'lastName': 'learnqa',
-            'email': email
-        }
+        data = self.prepeare_registration_data(email)
 
         response = requests.post('https://playground.learnqa.ru/api/user/', data=data)
 
