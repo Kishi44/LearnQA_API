08709443653098e6269ce5762e39e1390b2b5135
Kishi44 <kishi321@gmail.com>
2024-06-08 16:57:05 +0300

    HW 16 new tests get details

--- tests_lesson/test_user_get.py
+++ tests_lesson/test_user_get.py
@@ -29,4 +29,23 @@ class TestUserGet(BaseCase):
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.asser_json_has_keys(response2, expected_fields)
 
+    def test_get_user_details_for_another_user(self):
+        data = {
+            'email': 'vinkotov@example.com',
+            'password': '1234'
+        }
+        response1 = MyRequests.post("/user/login", data=data)
+
+        auth_sid = self.get_cookie(response1, 'auth_sid')
+        token = self.get_header(response1, 'x-csrf-token')
+        user_id = self.get_json_value(response1, "user_id") - 1
+
+        response2 = MyRequests.get(f"/user/{user_id}", headers={"x-csrf-token": token},
+                                   cookies={"auth_sid": auth_sid})
+
+        Assertions.asser_json_has_key(response2, "username")
+        Assertions.asser_json_has_not_key(response2, "email")
+        Assertions.asser_json_has_not_key(response2, "firstName")
+        Assertions.asser_json_has_not_key(response2, "lastName")
+
 
